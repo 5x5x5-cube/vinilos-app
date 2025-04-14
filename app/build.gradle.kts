28a59e61835b3606910b1c65b4aa6b0e4dae44dc
racarrascal <racarrascal@sprc.com.co>
2025-04-14 13:44:34 -0500

    Primer commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.example.vinillosapp"
+    namespace = "com.uniandes.vinillosapp"
     compileSdk = 35
 
+
     defaultConfig {
-        applicationId = "com.example.vinillosapp"
+        applicationId = "com.uniandes.vinillosapp"
         minSdk = 21
         targetSdk = 35
         versionCode = 1
@@ -33,6 +35,11 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+
 }
 
 dependencies {
@@ -40,7 +47,24 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.volley)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
---
+++ app/src/main/java/com/uniandes/vinilosapp/models/Comment.kt
@@ -0,0 +1,7 @@
+package com.uniandes.vinilosapp.models
+
+data class Comment (
+    val description:String,
+    val rating:String,
+    val albumId:Int
+)
