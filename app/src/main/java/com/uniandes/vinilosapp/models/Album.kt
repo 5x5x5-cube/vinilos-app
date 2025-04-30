8b3284705a80ac855da98c5c716aecae545cb68a
rcarrascalc <r.carrascalc@uniandes.edu.co>
2025-04-30 12:43:18 -0500

    Agregar DB en albunes (#76)
    * Agregar DB en albunes
    
    * Agregar DB en albumes
    
    ---------
    
    Co-authored-by: racarrascal <racarrascal@sprc.com.co>

--- app/src/main/java/com/uniandes/vinilosapp/models/Album.kt
+++ app/src/main/java/com/uniandes/vinilosapp/models/Album.kt
@@ -1,11 +1,15 @@
 package com.uniandes.vinilosapp.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tb_albums")
 data class Album (
-    val albumId:Int,
+    @PrimaryKey val albumId:Int,
     val name:String,
     val cover:String,
     val releaseDate:String,
     val description:String,
     val genre:String,
-    val recordLabel:String
+    val recordLabel:String,
 )
--- app/src/main/java/com/uniandes/vinilosapp/repositories/AlbumRepository.kt
+++ app/src/main/java/com/uniandes/vinilosapp/repositories/AlbumRepository.kt
@@ -1,13 +1,25 @@
 package com.uniandes.vinilosapp.repositories
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import com.uniandes.vinilosapp.database.AlbumDao
 import com.uniandes.vinilosapp.models.Album
 import com.uniandes.vinilosapp.models.AlbumDetails
 import com.uniandes.vinilosapp.network.NetworkServiceAdapter
 
-class AlbumRepository (val application: Application){
+class AlbumRepository (val application: Application,  private val albumDao: AlbumDao){
+
+
     suspend fun getAlbums(): List<Album> {
-        return NetworkServiceAdapter.getInstance(application).getAlbums()
+        var cached = albumDao.getAlbums()
+        return if (cached.isNullOrEmpty()) {
+            val cm =
+                application.baseContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_WIFI && cm.activeNetworkInfo?.type != ConnectivityManager.TYPE_MOBILE) {
+                emptyList()
+            } else NetworkServiceAdapter.getInstance(application).getAlbums()
+        } else cached
     }
 
     suspend fun getAlbum(albumId:Int): AlbumDetails {
