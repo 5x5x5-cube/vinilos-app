d95988fc6270de350ee1dfe17598ca11b61e2f15
Andrés Romero <a.romerop2@uniandes.edu.co>
2025-05-02 15:52:05 -0400

    Pruebas E2E HU03, HU04 y HU05

--- app/src/androidTest/java/com/uniandes/vinilosapp/PerformerDetailTest.kt
+++ app/src/androidTest/java/com/uniandes/vinilosapp/PerformerDetailTest.kt
@@ -7,17 +7,17 @@ import com.uniandes.vinilosapp.views.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlinx.coroutines.delay
-import androidx.compose.ui.test.junit4.ComposeTestRule
 
 @RunWith(AndroidJUnit4::class)
-class AlbumDetailTest {
+class PerformerDetailTest {
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun navigateToAlbumDetail_andShowTitle() {
+    fun navigateToPerformerDetail_andShowDescription() {
+
+        composeTestRule.onNodeWithContentDescription("Artists", useUnmergedTree = true).performClick()
 
         composeTestRule.waitUntil(timeoutMillis = 8000) {
             composeTestRule.onAllNodesWithText("Ver").fetchSemanticsNodes().isNotEmpty()
@@ -26,14 +26,16 @@ class AlbumDetailTest {
         composeTestRule.onAllNodesWithText("Ver").onFirst().performClick()
 
         composeTestRule.waitUntil(timeoutMillis = 8000) {
-            composeTestRule.onAllNodes(hasText("Buscando América", substring = true)).fetchSemanticsNodes().isNotEmpty()
+            composeTestRule.onAllNodes(hasText("Descripcion", substring = true)).fetchSemanticsNodes().isNotEmpty()
         }
 
-        composeTestRule.onAllNodesWithText("Buscando América", substring = true).onFirst().assertIsDisplayed()
+        composeTestRule.onAllNodes(hasText("Descripcion", substring = true)).onFirst().assertExists()
     }
 
     @Test
-    fun albumDetail_displaysTrack() {
+    fun navigateToPerformerDetail_andGoBackToList() {
+        composeTestRule.onNodeWithContentDescription("Artists", useUnmergedTree = true).performClick()
+
         composeTestRule.waitUntil(timeoutMillis = 8000) {
             composeTestRule.onAllNodesWithText("Ver").fetchSemanticsNodes().isNotEmpty()
         }
@@ -41,22 +43,7 @@ class AlbumDetailTest {
         composeTestRule.onAllNodesWithText("Ver").onFirst().performClick()
 
         composeTestRule.waitUntil(timeoutMillis = 8000) {
-            composeTestRule.onAllNodes(hasText("Decisiones", substring = true)).fetchSemanticsNodes().isNotEmpty()
-        }
-
-        composeTestRule.onNodeWithText("Decisiones").performScrollTo().assertIsDisplayed()
-    }
-
-    @Test
-    fun albumDetail_goBackToList() {
-        composeTestRule.waitUntil(timeoutMillis = 8000) {
-            composeTestRule.onAllNodesWithText("Ver").fetchSemanticsNodes().isNotEmpty()
-        }
-
-        composeTestRule.onAllNodesWithText("Ver").onFirst().performClick()
-
-        composeTestRule.waitUntil(timeoutMillis = 8000) {
-            composeTestRule.onAllNodes(hasText("Descripción", substring = true)).fetchSemanticsNodes().isNotEmpty()
+            composeTestRule.onAllNodes(hasText("Descripcion", substring = true)).fetchSemanticsNodes().isNotEmpty()
         }
 
         composeTestRule.onNodeWithContentDescription("Go back").performClick()
